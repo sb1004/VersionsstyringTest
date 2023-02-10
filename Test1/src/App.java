d40e605a26b60142d94ed27222f25bcb3f79833b
simon <simon_bruslund@hotmail.com>
2023-02-10 09:38:46 +0100

    Added awesome statements

--- Test1/src/App.java
+++ Test1/src/App.java
@@ -2,5 +2,10 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello verden world");
         System.out.println("Ja");
+
+        // Her følger en masse lækre udsagn
+        System.out.println("Ost er godt");
+        System.out.println("Vafler til aftensmad");
+        System.out.println("PIZZA!!");
     }
 }
